001ba46f9cf1f0587f586cecf60956508d9af76a
Celeste <celeste.cavallin@hotmail.fr>
2026-04-21 16:30:52 +0200

    crud book suite

--- src/main/java/tricycle/bookHub/service/BookService.java
+++ src/main/java/tricycle/bookHub/service/BookService.java
@@ -1,22 +1,18 @@
 package tricycle.bookHub.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import tricycle.bookHub.exception.BookNotFoundException;
 import tricycle.bookHub.model.Book;
 import tricycle.bookHub.repository.BookRepository;
 
-import javax.crypto.spec.OAEPParameterSpec;
 import java.util.List;
-import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class BookService {
 
-    private BookRepository repository;
-
-    public BookService(BookRepository repository) {
-        this.repository = repository;
-    }
+    private final BookRepository repository;
 
     public Book addBook(Book book){
        return repository.save(book);
@@ -26,22 +22,32 @@ public class BookService {
        return repository.findAll();
     }
 
-    public Optional<Book> getBookById(long id){
-        return repository.findById(id);
+    public Book getBookById(long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException("Ce livre avec cet id: "+ id + " n'existe pas"));
     }
 
     public Book updateBook(Book book, Long id){
-        if(repository.findById(id).isEmpty()) {
-            throw new BookNotFoundException("Ce livre avec cet id: "+ id + " n'existe pas");
-        }
-        return repository.save(book);
+        Book existingBook = repository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException("Ce livre avec cet id: "+ id + " n'existe pas"));
+
+        existingBook.setTitle(book.getTitle());
+        existingBook.setAuthor(book.getAuthor());
+        existingBook.setDescription(book.getDescription());
+        existingBook.setISBN(book.getISBN());
+        existingBook.setCover(book.getCover());
+        existingBook.setAvailable(book.isAvailable());
+        existingBook.setState(book.getState());
+        existingBook.setCategory(book.getCategory());
+
+        return repository.save(existingBook);
     }
 
-    public void deleteBookById(Book book, long id){
-        if(repository.findById(id).isEmpty()) {
-            throw new BookNotFoundException("Ce livre avec cet id: "+ id + " n'existe pas");
-        }
-        repository.delete(book);
+    public void deleteBookById(long id){
+        Book existingBook = repository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException("Ce livre avec cet id: "+ id + " n'existe pas"));
+
+        repository.delete(existingBook);
     }
 
  }
