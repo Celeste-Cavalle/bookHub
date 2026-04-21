2c10cb09cb61800e47dbb47c1f60b52db0c3ae8f
Sebastien LALOE <sebastien.laloe2024@campus-eni.fr>
2026-04-21 11:55:04 +0200

    ENTITY - Création et vérification entitées

---
+++ src/main/java/tricycle/bookHub/model/Category.java
@@ -0,0 +1,23 @@
+package tricycle.bookHub.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(name = "libelle")
+    private String libelle;
+
+}
