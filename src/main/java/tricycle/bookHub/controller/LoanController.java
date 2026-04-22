002dceed48294e5a787dde21a36ac5b90f2fd0a8
Celeste <celeste.cavallin@hotmail.fr>
2026-04-22 14:02:30 +0200

    crud modifs

--- src/main/java/tricycle/bookHub/controller/LoanController.java
+++ src/main/java/tricycle/bookHub/controller/LoanController.java
@@ -20,16 +20,19 @@ public class LoanController {
     @PostMapping("/api/loans")
     public ResponseEntity<Loan> addLoan(@RequestBody Loan loan){
         Loan savedLoan = service.addLoan(loan);
-        URI location = URI.create("/api/loans" + savedLoan.getId());
+        URI location = URI.create("/api/loans/" + savedLoan.getId());
 
         return ResponseEntity.created(location).body(savedLoan);
     }
 
     @GetMapping("/api/loans/my")
     public ResponseEntity<List<Loan>> getLoansById(@AuthenticationPrincipal User user){
+        if (user == null){
+            return ResponseEntity.status(401).build();
+        }
+
         Long userId = user.getId();
         List<Loan> loans = service.getLoanByUserID(userId);
-
         return ResponseEntity.ok(loans);
     }
 
